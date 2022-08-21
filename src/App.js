e14758f2a70889b5b9019d37a8cd089f841b4f81
Mrinal <mail.mrinal10@gmail.com>
2022-08-21 19:05:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FragmentDemo from './Components/FragmentDemo';
+
+function App() {
+  return (
+    <div className="App">
+      <FragmentDemo />
+    </div>
+  );
+}
+
+export default App;
