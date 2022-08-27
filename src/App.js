8ff0efab6ca6977e0603b111e876f481a8a97099
Mrinal <mail.mrinal10@gmail.com>
2022-08-27 11:54:29 +0530

    portals

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import ParentComp from './Components/ParentComp';
+import PortalDemo from './Components/PortalDemo';
 
 function App() {
   return (
     <div className="App">
       {/* <Table /> */}
       <ParentComp />
+      <PortalDemo />
     </div>
   );
 }
